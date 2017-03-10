078511de72af65d7b80fb974b9b5211fc6652cff
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-03-09 16:26:09 -0800

    update ./ruby

--- ruby/the_office_1_outed.rb
+++ ruby/the_office_1_outed.rb
@@ -1,5 +1,6 @@
 meet = {"tim":0, "jim":2, "randy":0, "sandy":7, "andy":0, "katie":5, "laura":1, "saajid":2, "alex":3, "john":2, "mr":0}
 boss = "laura"
+#sample data
 
 def outed(meet, boss)
   vibe = 0
